76dbb88e895d268e1b95742d9d79a0ea0fd5732d
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-08-21 09:55:10 +0530

    Add files via upload

--- menu driven/menu.sh
+++ menu driven/menu.sh
@@ -1,96 +1,151 @@
-#shell script to perform menu driven program to display
+#menudriven program
 opt=0
 while [ $opt -le 7 ]
 do
-clear
-echo "\n1.all ordinary files\n2.all directory files"
-echo "\n3.all special files\n4.all files readable to ugo"
-echo "\n5all files writeable to ugo\n6.all files executable to ugo"
-echo "\n7.all rwx files\n8.exit"
-echo "\noption :"
+echo "1.All ordinary files \n2.All Directory files \n3.All special files \n4.All files readable to ugo \n5.All files writable to ugo \n6.All files executable to ugo \n7.All files with rwx to owner \n8.exit"
+echo "\nEnter your Option: "
 read opt
 case $opt in
-1)for f in *
-  do
-  if [ -f $f ]
-  then
+1)
+count=0
+echo "\n\n\n-----ALL ORDINARY FILES-----"
+for f in *
+do
+if [ -f $f ]
+then
+count=$count+1
+echo $f
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+2)
+count=0
+echo "\n\n\n-----ALL DIRECTORY FILES-----"
+for f in *
+do
+if [ -d $f ]
+then
+count=$count+1
+echo $f
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+3)
+count=0
+echo "\n\n\n-----ALL SPECIAL FILES-----"
+for f in *
+do
+if [ ! -d $f -a ! -f $f ]
+then
+count=$count+1
+echo $f
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+4)
+count=0
+echo "\n\n\n-----ALL FILES READABLE TO UGO-----"
+for f in *
+do
+if [ -f $f ]
+then
+u=`ls -l $f | cut -c 2-2`
+g=`ls -l $f | cut -c 5-5`
+o=`ls -l $f | cut -c 8-8`
+if [ $u = "r" -a $g = "r" -a $o = "r" ]
+then 
+count=$count+1
+echo $f
+fi
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+
+5)
+count=0
+echo "\n\n\n-----ALL FILES WRITABLE TO UGO-----"
+for f in *
+do
+if [ -f $f ] 
+then
+u=`ls -l $f | cut -c 3-3`
+g=`ls -l $f | cut -c 6-6`
+o=`ls -l $f | cut -c 9-9`
+if [ $u = "w" -a $g = "w" -a $o = "w" ]
+then 
+count=$count+1
+echo $f
+fi
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+
+6)
+count=0
+echo "\n\n\n-----ALL FILES EXECUTABLE TO UGO-----"
+for f in *
+do
+if [ -f $f ] 
+then
+u=`ls -l $f | cut -c 4-4`
+g=`ls -l $f | cut -c 7-7`
+o=`ls -l $f | cut -c 10-10`
+if [ $u = "x" -a $g = "x" -a $o = "x" ]
+then 
+count=$count+1
+echo $f
+fi
+fi
+done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+
+7)
+count=0
+echo "\n\n\n-----ALL FILES RWX TO OWNER-----"
+for f in *
+do
+if [ -f $f ]
+then
+rwx=` ls -l $f | cut -c 2-4`
+if [ $rwx = "rwx" ]
+then
+count=$count+1
   echo $f
-  fi
-  done
-  ;;
-2)for f in *
-  do
-  if [ -d $f ]
-  then
-  echo $f
-  fi
-  done
-  ;;
-3)for f in *
-  do
-  if [ ! -d $f -a ! -f $f ]
-  then
-  echo $f
-  fi
-  done
-  ;;
-4)for f in *
-  do
-  if [ -f $f ]
-  then
-  u=`ls -l $f | cut -c 2-2`
-  g=`ls -l $f | cut -c 5-5`
-  o=`ls -l $f | cut -c 8-8`
-  if [ $u="r" -a $g="r" -a $o="r" ]
-  then
-  echo $f
-  fi
-  fi
-  done
-  ;;
-5)for f in *
-  do
-  if [ -f $f ]
-  then
-  u=`ls -l $f | cut -c 3-3`
-  g=`ls -l $f | cut -c 6-6`
-  o=`ls -l $f | cut -c 9-9`
-  if [ $u="w" -a $g="w" -a $o="w" ]
-  then
-  echo $f
-  fi
-  fi
-  done
-  ;;
-6)for f in *
-  do
-  if [ -f $f ]
-  then
-  u=`ls -l $f | cut -c 4-4`
-  g=`ls -l $f | cut -c 7-7`
-  o=`ls -l $f | cut -c 10-10`
-  if [ $u="x" -a $g="x" -a $o="x" ]
-  then
-  echo $f
-  fi
-  fi
-  done
-  ;;
-7)for f in *
-  do
-  if [ -f $f ]
-  then
-  rwx=`ls -l $f | cut -c 2-4`
-  if [ $rwx="rwx" ]
-  then
-  echo $f
-  fi
-  fi
-  done
-  ;;
-8)exit 0
-  ;;
+fi
+fi
+ done
+if [ $count = 0 ]
+then
+echo "No files exist"
+fi
+;;
+8) exit 0
+;;
 esac
-echo "Enter to continue......."
+echo "\nPress enter to continue"
 read x
 done
